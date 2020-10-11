dbaf27ce82dc14d0d161b991e29d8cd3f37aadc7
edersonwalendorff <62076214+edersonwalendorff@users.noreply.github.com>
2020-10-11 18:31:33 -0300

    Ederson
    First commit

---
+++ EditorDeTexto/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace EditorDeTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.italico = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.negrito = new System.Windows.Forms.PictureBox();
+            this.fontSize = new System.Windows.Forms.PictureBox();
+            this.novoArquivo = new System.Windows.Forms.PictureBox();
+            this.centralizar = new System.Windows.Forms.PictureBox();
+            this.alinharDireita = new System.Windows.Forms.PictureBox();
+            this.justificar = new System.Windows.Forms.PictureBox();
+            this.alinharEsquerda = new System.Windows.Forms.PictureBox();
+            this.sbtnSave = new System.Windows.Forms.PictureBox();
+            this.fonte = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pagina = new System.Windows.Forms.RichTextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.italico)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.negrito)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fontSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.novoArquivo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.centralizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alinharDireita)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.justificar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alinharEsquerda)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sbtnSave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fonte)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.OldLace;
+            this.panel1.Controls.Add(this.italico);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.negrito);
+            this.panel1.Controls.Add(this.fontSize);
+            this.panel1.Controls.Add(this.novoArquivo);
+            this.panel1.Controls.Add(this.centralizar);
+            this.panel1.Controls.Add(this.alinharDireita);
+            this.panel1.Controls.Add(this.justificar);
+            this.panel1.Controls.Add(this.alinharEsquerda);
+            this.panel1.Controls.Add(this.sbtnSave);
+            this.panel1.Controls.Add(this.fonte);
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.panel1.Size = new System.Drawing.Size(888, 84);
+            this.panel1.TabIndex = 0;
+            // 
+            // italico
+            // 
+            this.italico.Image = ((System.Drawing.Image)(resources.GetObject("italico.Image")));
+            this.italico.Location = new System.Drawing.Point(471, 10);
+            this.italico.Name = "italico";
+            this.italico.Size = new System.Drawing.Size(30, 30);
+            this.italico.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.italico.TabIndex = 14;
+            this.italico.TabStop = false;
+            this.italico.Click += new System.EventHandler(this.italico_Click);
+            this.italico.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(26, 28);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // negrito
+            // 
+            this.negrito.Image = ((System.Drawing.Image)(resources.GetObject("negrito.Image")));
+            this.negrito.Location = new System.Drawing.Point(435, 10);
+            this.negrito.Name = "negrito";
+            this.negrito.Size = new System.Drawing.Size(30, 28);
+            this.negrito.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.negrito.TabIndex = 12;
+            this.negrito.TabStop = false;
+            this.negrito.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            // 
+            // fontSize
+            // 
+            this.fontSize.Image = ((System.Drawing.Image)(resources.GetObject("fontSize.Image")));
+            this.fontSize.Location = new System.Drawing.Point(399, 10);
+            this.fontSize.Name = "fontSize";
+            this.fontSize.Size = new System.Drawing.Size(30, 30);
+            this.fontSize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.fontSize.TabIndex = 11;
+            this.fontSize.TabStop = false;
+            this.fontSize.MouseEnter += new System.EventHandler(this.btnExit_MouseEnter);
+            this.fontSize.MouseLeave += new System.EventHandler(this.btnExit_MouseLeave);
+            // 
+            // novoArquivo
+            // 
+            this.novoArquivo.Image = ((System.Drawing.Image)(resources.GetObject("novoArquivo.Image")));
+            this.novoArquivo.Location = new System.Drawing.Point(12, 12);
+            this.novoArquivo.Name = "novoArquivo";
+            this.novoArquivo.Size = new System.Drawing.Size(25, 28);
+            this.novoArquivo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.novoArquivo.TabIndex = 10;
+            this.novoArquivo.TabStop = false;
+            this.novoArquivo.Click += new System.EventHandler(this.novoArquivo_Click);
+            this.novoArquivo.MouseEnter += new System.EventHandler(this.novoArquivo_MouseEnter);
+            this.novoArquivo.MouseLeave += new System.EventHandler(this.novoArquivo_MouseLeave);
+            // 
+            // centralizar
+            // 
+            this.centralizar.Image = ((System.Drawing.Image)(resources.GetObject("centralizar.Image")));
+            this.centralizar.Location = new System.Drawing.Point(471, 44);
+            this.centralizar.Name = "centralizar";
+            this.centralizar.Size = new System.Drawing.Size(30, 30);
+            this.centralizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.centralizar.TabIndex = 9;
+            this.centralizar.TabStop = false;
+            this.centralizar.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            // 
+            // alinharDireita
+            // 
+            this.alinharDireita.Image = ((System.Drawing.Image)(resources.GetObject("alinharDireita.Image")));
+            this.alinharDireita.Location = new System.Drawing.Point(435, 44);
+            this.alinharDireita.Name = "alinharDireita";
+            this.alinharDireita.Size = new System.Drawing.Size(30, 30);
+            this.alinharDireita.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.alinharDireita.TabIndex = 8;
+            this.alinharDireita.TabStop = false;
+            this.alinharDireita.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            // 
+            // justificar
+            // 
+            this.justificar.Image = ((System.Drawing.Image)(resources.GetObject("justificar.Image")));
+            this.justificar.Location = new System.Drawing.Point(399, 44);
+            this.justificar.Name = "justificar";
+            this.justificar.Size = new System.Drawing.Size(30, 30);
+            this.justificar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.justificar.TabIndex = 7;
+            this.justificar.TabStop = false;
+            this.justificar.MouseEnter += new System.EventHandler(this.pictureBox2_MouseEnter);
+            // 
+            // alinharEsquerda
+            // 
+            this.alinharEsquerda.Image = ((System.Drawing.Image)(resources.GetObject("alinharEsquerda.Image")));
+            this.alinharEsquerda.Location = new System.Drawing.Point(363, 44);
+            this.alinharEsquerda.Name = "alinharEsquerda";
+            this.alinharEsquerda.Size = new System.Drawing.Size(30, 30);
+            this.alinharEsquerda.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.alinharEsquerda.TabIndex = 6;
+            this.alinharEsquerda.TabStop = false;
+            this.alinharEsquerda.Click += new System.EventHandler(this.alinharEsquerda_Click);
+            this.alinharEsquerda.MouseEnter += new System.EventHandler(this.alinharEsquerda_MouseEnter);
+            // 
+            // sbtnSave
+            // 
+            this.sbtnSave.Image = ((System.Drawing.Image)(resources.GetObject("sbtnSave.Image")));
+            this.sbtnSave.Location = new System.Drawing.Point(849, 38);
+            this.sbtnSave.Name = "sbtnSave";
+            this.sbtnSave.Size = new System.Drawing.Size(25, 28);
+            this.sbtnSave.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sbtnSave.TabIndex = 5;
+            this.sbtnSave.TabStop = false;
+            this.sbtnSave.MouseEnter += new System.EventHandler(this.sbtnSave_MouseEnter);
+            this.sbtnSave.MouseLeave += new System.EventHandler(this.sbtnSave_MouseLeave);
+            // 
+            // fonte
+            // 
+            this.fonte.Image = ((System.Drawing.Image)(resources.GetObject("fonte.Image")));
+            this.fonte.Location = new System.Drawing.Point(363, 10);
+            this.fonte.Name = "fonte";
+            this.fonte.Size = new System.Drawing.Size(30, 30);
+            this.fonte.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.fonte.TabIndex = 4;
+            this.fonte.TabStop = false;
+            this.fonte.MouseEnter += new System.EventHandler(this.fonte_MouseEnter);
+            this.fonte.MouseLeave += new System.EventHandler(this.fonte_MouseLeave);
+            this.fonte.MouseMove += new System.Windows.Forms.MouseEventHandler(this.fonte_MouseMove);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.Location = new System.Drawing.Point(851, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(20, 20);
+            this.btnExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnExit.TabIndex = 3;
+            this.btnExit.TabStop = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.btnExit.MouseEnter += new System.EventHandler(this.btnExit_MouseEnter);
+            this.btnExit.MouseLeave += new System.EventHandler(this.btnExit_MouseLeave);
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 84);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(888, 28);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.OldLace;
+            this.panel3.Controls.Add(this.pagina);
+            this.panel3.Location = new System.Drawing.Point(164, 106);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(542, 662);
+            this.panel3.TabIndex = 2;
+            // 
+            // pagina
+            // 
+            this.pagina.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.pagina.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pagina.Location = new System.Drawing.Point(3, 3);
+            this.pagina.Name = "pagina";
+            this.pagina.Size = new System.Drawing.Size(536, 656);
+            this.pagina.TabIndex = 0;
+            this.pagina.Text = "";
+            this.pagina.TextChanged += new System.EventHandler(this.pagina_TextChanged);
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 766);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(888, 34);
+            this.panel4.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(888, 800);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.italico)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.negrito)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fontSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.novoArquivo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.centralizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alinharDireita)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.justificar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alinharEsquerda)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sbtnSave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fonte)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox btnExit;
+        private System.Windows.Forms.PictureBox fonte;
+        private System.Windows.Forms.PictureBox sbtnSave;
+        private System.Windows.Forms.RichTextBox pagina;
+        private System.Windows.Forms.PictureBox justificar;
+        private System.Windows.Forms.PictureBox alinharEsquerda;
+        private System.Windows.Forms.PictureBox centralizar;
+        private System.Windows.Forms.PictureBox alinharDireita;
+        private System.Windows.Forms.PictureBox novoArquivo;
+        private System.Windows.Forms.PictureBox italico;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox negrito;
+        private System.Windows.Forms.PictureBox fontSize;
+    }
+}
+
---
+++ EditorDeTexto/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+
+namespace EditorDeTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void OnHouverSound()
+        {
+            SoundPlayer houverSound = new SoundPlayer(@"C:\Users\João\source\repos\EditorDeTexto\EditorDeTexto\resources\sounds/audioOnHouver.wav");
+            houverSound.Play();
+            
+        }
+      
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void btnExit_MouseEnter(object sender, EventArgs e)
+        {
+         
+            btnExit.Size = new Size(22, 22);
+             OnHouverSound();
+        }
+
+        private void btnExit_MouseLeave(object sender, EventArgs e)
+        {
+           btnExit.Size = new Size(20, 20);
+
+
+        }
+        private void sbtnSave_MouseEnter(object sender, EventArgs e)
+        {
+            sbtnSave.Size = new Size(27, 26);
+            OnHouverSound();
+        }
+
+        private void sbtnSave_MouseLeave(object sender, EventArgs e)
+        {
+            sbtnSave.Size = new Size(25, 24);
+
+        }
+
+        private void pagina_TextChanged(object sender, EventArgs e)
+        {
+            pagina.SelectionStart = pagina.Text.Length;
+            pagina.ScrollToCaret();
+
+        }
+
+        private void btnExit_MouseClick(object sender, MouseEventArgs e)
+        {
+            btnExit.Size = new Size(20, 20);
+
+        }
+
+        private void fonte_MouseEnter(object sender, EventArgs e)
+        {
+           
+            fonte.Size = new Size(32, 32);
+            OnHouverSound();
+
+        }
+        private void fonte_MouseLeave(object sender, EventArgs e)
+        {
+            fonte.Size = new Size(30, 30);
+
+        }
+
+
+
+     
+
+
+
+
+
+
+
+
+        private void italico_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void novoArquivo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void novoArquivo_MouseLeave(object sender, EventArgs e)
+        {
+            novoArquivo.Size = new Size(24, 28);
+        }
+
+        private void novoArquivo_MouseEnter(object sender, EventArgs e)
+        {
+            novoArquivo.Size = new Size(26, 30);
+            OnHouverSound();
+        }
+
+        private void fonte_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size(26, 30);
+            OnHouverSound();
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size(24, 28);
+        }
+
+        private void alinharEsquerda_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void alinharEsquerda_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
